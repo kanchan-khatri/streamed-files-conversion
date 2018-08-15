c5c7ea9e73cb45381bb848ba6b69dc4288c797d1
oleksii <oleksiyprosyanko@gmail.com>
2018-08-15 14:01:25 +0300

    Add scala doc

--- src/main/scala/op/assessment/ebay/CreditLimit.scala
+++ src/main/scala/op/assessment/ebay/CreditLimit.scala
@@ -2,40 +2,41 @@ package op.assessment.ebay
 
 object CreditLimit {
 
-  final case class Record(
-      name: String,
-      address: String,
-      postcode: String,
-      phone: String,
-      limit: String,
-      birthday: String
-    )
+	final case class Record(
+			name: String,
+			address: String,
+			postcode: String,
+			phone: String,
+			limit: String,
+			birthday: String
+		)
 
-  type RowParse = String => Option[Record]
+	type RowParse = String => Option[Record]
 
 	/**
 		* Try to determine a type of a workbook and get corresponding parser.
+		*
 		* @param path path to workbook.
 		* @return parse function for a type determined
 		*/
-  def workBookParse(path: String): RowParse = path match {
-    case csv if csv.endsWith(".csv") => parse[CsvType]
-    case prn if prn.endsWith(".prn") => parse[TsvType]
-    case _ => _ => None
-  }
+	def workBookParse(path: String): RowParse = path match {
+		case csv if csv.endsWith(".csv") => parse[CsvType]
+		case prn if prn.endsWith(".prn") => parse[TsvType]
+		case _ => _ => None
+	}
 
-	private def parse[F <: FormatType: RowFormat]: RowParse = {
+	private def parse[F <: FormatType : RowFormat]: RowParse = {
 		def toRecord(formatted: Option[List[String]]): Option[Record] =
 			formatted.map(_.filter(_.nonEmpty)).filter(_.size == 6) map {
 				line => {
 					val fields = line.toVector
 					Record(
-						name			= fields(0),
-						address		= fields(1),
-						postcode	= fields(2),
-						phone			= fields(3),
-						limit			= fields(4),
-						birthday 	= fields(5))
+						name = fields(0),
+						address = fields(1),
+						postcode = fields(2),
+						phone = fields(3),
+						limit = fields(4),
+						birthday = fields(5))
 				}
 			}
 
